93f1e9dab959258a2dba14067bfb4db3e449c49f
Brede-dev <liadal15@kingshigh.school.nz>
2025-08-14 14:55:38 +1200

    secret

--- Scripts/character_body_2d.gd
+++ Scripts/character_body_2d.gd
@@ -215,6 +215,14 @@ func _on_hit_box_area_entered(area: Area2D) -> void:
 		$".".position = $"../Flags".position  # Add .position to get the Vector2
 		$DeathSound.play()
 	elif area.is_in_group("win"):
-		get_tree().change_scene_to_file("res://win_screen.tscn")
+		# Check if gubgub is visible before deciding which win screen to show
+		var gubgub_sprite = $gubgub  # Direct reference since gubgub is a child of Player
+		
+		if gubgub_sprite and gubgub_sprite.visible:
+			print("Gubgub is visible! Going to special win screen!")
+			get_tree().change_scene_to_file("res://gubby_win.tscn")
+		else:
+			print("Gubgub not visible, going to normal win screen")
+			get_tree().change_scene_to_file("res://win_screen.tscn")
 	else:
 		get_tree().change_scene_to_file("res://death_screen.tscn")
--- delivery_area.gd
+++ delivery_area.gd
@@ -1,14 +1,29 @@
 extends Area2D
 
+func _ready():
+	# Make sure the signal is connected
+	body_entered.connect(_on_body_entered)
+	print("DeliveryArea ready - signal connected!")
+
 func _on_body_entered(body: Node2D) -> void:
+	print("Something entered DeliveryArea: ", body.name)
+	
 	if body is CharacterBody2D:
-		# Find the gubgub sprite in the scene
-		var gubgub_sprite = get_tree().current_scene.find_child("gubgub", true, false)
+		print("Player entered delivery area!")
+		
+		# Find the gubgub sprite in the scene (it's under the Player node)
+		var gubgub_sprite = body.find_child("gubgub", true, false)
+		print("gubgub sprite found: ", gubgub_sprite != null)
+		
+		if gubgub_sprite:
+			print("gubgub visible: ", gubgub_sprite.visible)
 		
 		# Check if gubgub exists and is visible
 		if gubgub_sprite and gubgub_sprite.visible:
+			print("Taking player to gubby_win scene!")
 			# If gubgub is visible, go to gubby_win scene
 			get_tree().change_scene_to_file("res://gubby_win.tscn")
 		else:
+			print("Taking player to normal win screen")
 			# If gubgub is not visible or doesn't exist, go to normal win screen
 			get_tree().change_scene_to_file("res://win_screen.tscn")
